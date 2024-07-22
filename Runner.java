78b8ffa2188faf2a5ead4c5f5d91c946c52b9041
Amoyzzz <dharkarajveer@gmail.com>
2024-07-22 02:27:45 -0400

    Chess
    At last

--- Runner.java
+++
@@ -1,8 +0,0 @@
-    // Creates a new chessboard in the standard initial position
-    Board board = new Board();
-
-    //Make a move from E2 to E4 squares
-    board.doMove(new Move(Square.E2, Square.E4));
-
-    //print the chessboard in a human-readable form
-    System.out.println(board.toString());
