b915b33f921de640fc6c183318d55350e22e3dc9
Adi Suhit Jha <adi.suhit.jha@gmail.com>
2024-07-22 11:15:23 -0400

    some stuff

---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args){
+        JFrame window = new JFrame("Simple Chess");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+       window.setVisible(true); 
+    }
+}
